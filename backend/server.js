621dab1f17123b5d510d2080e4957bdebd1a663b
Shraddha <shraddhagadekar6265@gmail.com>
2026-02-28 19:55:43 +0530

    Updated authentication, admin dashboard, navbar username display

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const productRoutes = require("./routes/product");
-const cartRoutes = require("./routes/cart");
-const authRoutes = require("./routes/auth");
+require("dotenv").config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
 
-mongoose.connect("mongodb://127.0.0.1:27017/shop")
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/products", require("./routes/product"));
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
+// IMPORTANT FOR RENDER
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
